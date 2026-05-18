ec19fb8ae91082d4b958d5dd869a8dc84fc045ca
Rindra-Aniko <rindra.aniko@gmail.com>
2026-05-18 15:14:49 +0700

    feat: implement about page layout sections, update assets and page routes

---
+++ app/paket/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+
+export const metadata = {
+    title: "Paket",
+    description: "Paket",
+};
+
+export default function Paket() {
+    return (
+        <div>
+            <h1>Paket</h1>
+        </div>
+    );
+}
---
+++ app/portofolio/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+
+export const metadata = {
+    title: "Portofolio",
+    description: "Portofolio",
+};
+
+export default function Portofolio() {
+    return (
+        <div>
+            <h1>Portofolio</h1>
+        </div>
+    );
+}
