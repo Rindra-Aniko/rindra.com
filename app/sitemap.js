5c2c86deae1a9d17e4407ecaa729d5616c3198be
Rindra-Aniko <rindra.aniko@gmail.com>
2026-05-31 10:54:49 +0700

    google SITEMAP

--- app/sitemap.js
+++ app/sitemap.js
@@ -17,7 +17,7 @@ export default async function sitemap() {
   // Pemetaan URL dinamis untuk setiap artikel
   const articleUrls = allArticles.map((article) => ({
     url: `${baseUrl}/artikel/${article.slug}`,
-    lastModified: article.createdAt || new Date(),
+    lastModified: article.createdAt || new Date().toISOString().split('.')[0] + 'Z',
     changeFrequency: "weekly",
     priority: 0.8,
   }));
@@ -26,31 +26,31 @@ export default async function sitemap() {
   const staticUrls = [
     {
       url: `${baseUrl}`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split('.')[0] + 'Z',
       changeFrequency: "weekly",
       priority: 1.0,
     },
     {
       url: `${baseUrl}/about`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split('.')[0] + 'Z',
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/paket`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split('.')[0] + 'Z',
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/portofolio`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split('.')[0] + 'Z',
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${baseUrl}/artikel`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split('.')[0] + 'Z',
       changeFrequency: "daily",
       priority: 0.9,
     },
